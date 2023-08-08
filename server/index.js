bb37031f8a2e949ea2d690e9119a6a523155633e
AbdullahiAden <Abbe6152@gmail.com>
2023-08-08 15:58:40 +0200

    routes, auth

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import cookieParser from "cookie-parser";
+
 const app = express();
 dotenv.config();
 
@@ -16,6 +21,26 @@ const connect = () => {
       throw err;
     });
 };
+
+app.use(cookieParser());
+app.use(express.json());
+
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+
+// middleware for better error handling
+app.use((err, req, res, next) => {
+  // if we dont send specific error, default 500
+  const status = err.status || 500;
+  const message = err.message || "something went wrong!";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
 
 app.listen(8800, () => {
   connect();
