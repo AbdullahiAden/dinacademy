5605e56eb8e351619233e9e5201b8b565a8a5c14
AbdullahiAden <Abbe6152@gmail.com>
2023-08-12 15:17:13 +0200

    CRUD: updateUser with pass encryption upon update

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,30 @@
-export const user = (req, res) => {
-  res.json("user routes succes");
+import { createError } from "../error.js";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+export const updateUser = async (req, res, next) => {
+  // check if current user is same jwt id,user can update its own cred  & so can admin
+  if (req.params.id === req.user.id || req.user.isAdmin) {
+    // if password is updated, we encrypt it again
+    if (req.body.password) {
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(req.body.password, salt);
+      req.body.password = hash;
+    }
+
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (error) {}
+  } else {
+    return next(createError(403, "you can update only your account"));
+  }
 };
+export const deleteUser = (req, res, next) => {};
+export const getUser = async (req, res, next) => {};
