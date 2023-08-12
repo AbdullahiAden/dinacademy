cba83dd55f6990ca58c949daf61d5415d0b2b3b1
AbdullahiAden <Abbe6152@gmail.com>
2023-08-12 15:19:42 +0200

    user routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,16 @@
 import express from "express";
-import {} from "../controllers/user.js";
+import { deleteUser, getUser, updateUser } from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
+
+// update user
+router.put("/:id", verifyToken, updateUser);
+
+//delete user
+router.delete("/:id", deleteUser);
+
+//get a user
+router.get("/find/:id", getUser);
 
 export default router;
