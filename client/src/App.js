c965a626685aa950b4b66bd705b7e19e6a10953e
AbdullahiAden <Abbe6152@gmail.com>
2023-08-16 12:52:57 +0200

    changed styles to sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import HomePage from "./Pages/Home/HomePage";
+import LoginPage from "./Pages/login/LoginPage";
+import SignupPage from "./Pages/Signup/SignupPage";
 
 function App() {
-  return <div className="App">Din Academy</div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/signup" element={<SignupPage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
