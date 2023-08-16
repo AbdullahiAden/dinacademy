3137f7595f8d88ed8165301aba9399e222c5b8b0
AbdullahiAden <Abbe6152@gmail.com>
2023-08-16 19:35:14 +0200

    homepage responsive

--- client/src/Pages/Home/HomePage.jsx
+++ client/src/Pages/Home/HomePage.jsx
@@ -1,53 +1,24 @@
 import React from "react";
-import styled from "styled-components";
 import Navbar from "../../components/navbar/Navbar";
+import image from "../../img/image.jpg";
 
-const Container = styled.div`
-  color: white;
-  height: 100vh;
-`;
+import "./homePage.scss";
 
-const Wrapper = styled.div`
-  display: flex;
-  flex: 3;
-  border: 1px solid red;
-  margin: 15px 15px;
-  padding: 65px 45px;
-`;
-const IntroSec = styled.div`
-  flex: 3;
-  border: 1px solid red;
-  align-items: center;
-
-  font-weight: bolder;
-  font-size: 5rem;
-`;
-const SubIntroSec = styled.div`
-  font-weight: lighter;
-  font-size: 1rem;
-  padding: 25px 0 0 0;
-`;
-const Button = styled.button`
-  cursor: pointer;
-  border: 1px solid blue;
-  color: #09172d;
-  background-color: #00b2fe;
-  border-radius: 15px;
-  font-weight: bold;
-  font-size: 1.2rem;
-  padding: 17px 30px;
-`;
-const ImageSec = styled.div`
-  display: flex;
-  flex: 3;
-  border: 1px solid red;
-  align-items: center;
-`;
 const HomePage = () => {
   return (
-    <div>
+    <div className="homePage">
       <Navbar />
-      Home page
+      <div className="intro-wrapper">
+        <div className="intro-sec">
+          <div className="left-sec">
+            <h1>EMBARK ON YOUR JOURNEY OF SEEKING KNOWLEDGE WITH US</h1>
+            <p>MASTER THE QURAN AND THE ARABIC LANGUAGE </p>
+            <button>JOIN NOW</button>
+          </div>
+        </div>
+
+        <div className="image-sec"></div>
+      </div>
     </div>
   );
 };
