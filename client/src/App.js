f787bebd30c7a07f66d6689633742ba0a5d981ce
AbdullahiAden <Abbe6152@gmail.com>
2023-08-27 14:17:13 +0200

    router

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/Home/HomePage";
 import LoginPage from "./Pages/login/LoginPage";
 import SignupPage from "./Pages/Signup/SignupPage";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/signup" element={<SignupPage />}></Route>
-          <Route path="/login" element={<LoginPage />}></Route>
-        </Routes>
-      </Router>
+    <div className="App">
+      {/* <Navbar /> */}
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </div>
   );
 }
