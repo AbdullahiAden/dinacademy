cef31bebd98f6520c52ae644c6995129ee1ddb67
AbdullahiAden <Abbe6152@gmail.com>
2023-08-27 14:17:39 +0200

    homepage

--- client/src/Pages/Home/HomePage.jsx
+++ client/src/Pages/Home/HomePage.jsx
@@ -20,6 +20,62 @@ const HomePage = () => {
 
       <div>
         <h2>Browse Courses</h2>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem magnam
+        dicta atque possimus repudiandae qui, amet ipsa exercitationem
+        molestias, quam officiis quos voluptas accusantium libero deserunt.
+        Provident magnam, praesentium asperiores ex dignissimos ratione vero.
+        Saepe ullam repellat necessitatibus perferendis exercitationem ea
+        mollitia cum suscipit voluptatibus eum, soluta qui incidunt maxime
+        animi? Earum, eos! Reprehenderit voluptates eum totam, omnis unde
+        explicabo recusandae, eius obcaecati sint ab incidunt vero! Quasi
+        voluptatem minus odit adipisci saepe cupiditate quaerat sed dignissimos,
+        omnis incidunt consectetur perspiciatis, ab fugiat. Velit, deleniti
+        harum, sapiente, minus earum illo et incidunt dolorem similique amet
+        nemo. Reprehenderit, architecto! Odit molestias ipsam beatae, doloribus
+        doloremque consequatur debitis et voluptatem eaque, repudiandae rerum
+        alias. Maxime soluta fugit officia, suscipit natus nam earum illo est?
+        Dolores quo nam distinctio perferendis optio eius, doloribus saepe,
+        rerum fugit cum inventore explicabo. Sequi voluptatum earum vero, odio
+        similique at impedit mollitia commodi adipisci distinctio quae eveniet
+        ullam omnis. Omnis magnam earum, non ex cupiditate eum voluptatibus
+        libero nulla repellat velit! Totam itaque ut odit, alias maxime
+        similique soluta ad voluptate, aliquam exercitationem nihil facere
+        officia magnam delectus sunt esse commodi incidunt necessitatibus
+        ducimus fugit eos culpa omnis recusandae explicabo! Ducimus quibusdam
+        molestiae id porro hic et beatae nobis, eligendi cumque tempora
+        laboriosam similique corrupti. Officiis ab distinctio praesentium vel
+        laudantium tempora sint, reiciendis fuga. Officia quibusdam placeat hic.
+        Ipsam neque eligendi dolor at. Temporibus dolore, corporis, magnam
+        reiciendis error sunt consectetur tempora non doloribus ut nostrum velit
+        eum, consequatur officia corrupti consequuntur exercitationem inventore.
+        Vel fugit assumenda perspiciatis, iusto a placeat enim quasi rerum optio
+        rem quia aliquam at facere, illum esse animi doloribus. Nesciunt
+        exercitationem, inventore explicabo unde vitae iusto quam dignissimos
+        autem hic, ex quia tenetur sint accusantium! Reiciendis harum officiis
+        aliquid. Voluptate doloremque consequuntur, unde laboriosam praesentium
+        distinctio ratione inventore, et nobis sequi quia minima dolorum odio
+        cupiditate perferendis perspiciatis quaerat quisquam neque voluptatibus
+        autem hic ut tenetur suscipit! Voluptatibus iure accusamus dolor autem,
+        maxime recusandae cupiditate quasi. Cupiditate magnam, numquam dolores
+        at aspernatur, explicabo, non beatae temporibus minima voluptate
+        architecto provident ducimus eos corporis rerum in. Atque voluptatum
+        rerum totam! Obcaecati molestiae amet optio eveniet, ut unde laborum!
+        Nisi aut vero sit quo reiciendis laboriosam, exercitationem ducimus
+        fugiat dolor illo facere necessitatibus similique voluptatem dolorem
+        saepe nulla velit fugit et nemo ratione deserunt doloremque nostrum!
+        Dolorum corrupti quod tempora voluptas fugit eligendi ab voluptates
+        provident cum veniam? Maxime porro natus, a, molestiae soluta esse ea
+        quae deserunt eligendi harum aut corporis, dolore alias numquam impedit
+        dolor aliquam. Labore explicabo nemo dolores at nihil, doloremque veniam
+        id eveniet delectus vel numquam saepe! Culpa impedit rem illum velit
+        tempora praesentium error, ducimus dolores ad, earum et sunt eos
+        adipisci quisquam numquam quas quam quidem perspiciatis, quod esse ipsum
+        fuga porro facere tempore. Nam impedit ex sed. Aliquid, sunt.
+        Voluptatibus ex sint non quasi laboriosam, laborum, assumenda
+        perferendis voluptas ipsa voluptates accusantium voluptatum placeat eos.
+        Suscipit veniam quam incidunt tempore nam earum perspiciatis obcaecati
+        aperiam exercitationem aliquid ratione voluptates, aliquam fuga qui
+        culpa laboriosam. Nisi!
       </div>
     </div>
   );
