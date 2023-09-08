6e74ec24d7fc05db0916ac274974673818174cc8
AbdullahiAden <Abbe6152@gmail.com>
2023-09-08 23:29:06 +0200

    fixed cookie error not showing in browser

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -1,11 +1,12 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
 // no need for url since we use proxy in package, otherwise we would write server url
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:8800" });
+const baseQuery = fetchBaseQuery({ baseUrl: "" });
 
 export const apiSlice = createApi({
   baseQuery,
   tagTypes: ["User"],
   //userApiSlice will use injects endpoints here
   endpoints: (builder) => ({}),
+  credentials: "include", // shows cookie in browser
 });
