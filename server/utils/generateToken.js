f0a422f6d229345c7cb983672c92e91a53c2979f
AbdullahiAden <Abbe6152@gmail.com>
2023-09-08 23:11:10 +0200

    comments

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,19 +1,15 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
-  const token = jwt.sign(
-    // { userId: user._id, isAdmin: user.isAdmin },
-    { userId },
-    process.env.JWT,
-    { expiresIn: "30d" }
-  );
+  const token = jwt.sign({ userId }, process.env.JWT, {
+    expiresIn: "30d",
+  });
 
-  //save token into cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
-    maxAge: 30 * 24 * 60 * 60 * 1000,
+    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
+    sameSite: "strict", // Prevent CSRF attacks
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
 
