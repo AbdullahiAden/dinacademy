aae3f452c823401955d4ad8b0a0c3911a4a212d4
AbdullahiAden <Abbe6152@gmail.com>
2023-09-08 23:10:23 +0200

    redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import store from "./store";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
 );
