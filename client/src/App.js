285aa13588f8f9d18d84f60cf9cec4d831c4bd18
AbdullahiAden <Abbe6152@gmail.com>
2023-09-08 23:09:58 +0200

    react toastify to show error msg

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,22 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "./Pages/Home/HomePage";
-import LoginPage from "./Pages/login/LoginPage";
+import LoginPage from "./Pages/Login/LoginPage";
 import SignupPage from "./Pages/Signup/SignupPage";
 import Navbar from "./components/navbar/Navbar";
+
+// for showing error messages
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
       {/* <Navbar /> */}
+      <ToastContainer toastStyle={{ backgroundColor: "#00b2fe" }} />
       <Routes>
         <Route index element={<HomePage />} />
+
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
