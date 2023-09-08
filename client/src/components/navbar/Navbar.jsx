f6085c5941cfb1754ff1767f9bdda1ab8fae44a9
AbdullahiAden <Abbe6152@gmail.com>
2023-09-08 23:11:31 +0200

    logout functionality

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,16 +3,35 @@ import "./navbar.scss";
 import logo from "../../img/logo.png";
 
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+import { logout } from "../../slices/authSlice";
+import { useLogoutMutation } from "../../slices/usersApiSlice";
+import { toast } from "react-toastify";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const token = localStorage.getItem("jwt");
 
-  const navigate = useNavigate();
+  const { userInfo } = useSelector((state) => state.auth);
 
-  const logoutUser = () => {
-    localStorage.removeItem("jwt");
-    navigate("/login");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  // function to call the logout mutation
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      // logoutApiCall make api call to destroy cookie
+      const res = await logoutApiCall().unwrap();
+      //dispatch logout to clear local storage
+      dispatch(logout());
+      toast(res.message);
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -26,34 +45,36 @@ const Navbar = () => {
         <span></span>
       </div>
 
-      {!token && (
-        <ul className={menuOpen ? "open" : ""}>
-          <li>
-            <NavLink to="/signup" className="nav-links">
-              Signup
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/login" className="nav-links">
-              Login
-            </NavLink>
-          </li>
-        </ul>
-      )}
-
-      {token && (
-        <ul className={menuOpen ? "open" : ""}>
-          <li>
-            <NavLink to="/dashboard" className="nav-links">
-              Dashboard
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/login" className="nav-links" onClick={logoutUser}>
-              logout
-            </NavLink>
-          </li>
-        </ul>
+      {userInfo ? (
+        <>
+          <ul className={menuOpen ? "open" : ""}>
+            <li>
+              <NavLink to="/dashboard" className="nav-links">
+                dashboard
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-links" onClick={logoutHandler}>
+                logout
+              </NavLink>
+            </li>
+          </ul>
+        </>
+      ) : (
+        <>
+          <ul className={menuOpen ? "open" : ""}>
+            <li>
+              <NavLink to="/signup" className="nav-links">
+                Signup
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/login" className="nav-links">
+                Login
+              </NavLink>
+            </li>
+          </ul>
+        </>
       )}
     </nav>
   );
