394df5b09d70aa89ea790c577ea52d0d6404f977
AbdullahiAden <Abbe6152@gmail.com>
2023-09-15 23:01:14 +0200

    watch page

---
+++ client/src/Pages/Watch/Watch.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Player from "../../components/Player/Player";
+
+const Watch = () => {
+  return (
+    <div>
+      <Navbar />
+      Watch
+      <Player />
+    </div>
+  );
+};
+
+export default Watch;
