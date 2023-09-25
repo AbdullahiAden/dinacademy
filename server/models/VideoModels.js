e7824fb0b3f3460b43cb2d08093591ef8b0bebd2
AbdullahiAden <Abbe6152@gmail.com>
2023-09-25 14:26:22 +0200

    update

--- server/models/VideoModels.js
+++ server/models/VideoModels.js
@@ -4,17 +4,20 @@ const VideoSchema = new mongoose.Schema(
   {
     playlistId: {
       type: "string",
-      required: true,
+      // required: true,
     },
-    titel: {
+    title: {
       type: "string",
       required: true,
+      unique: true,
     },
     description: {
       type: "string",
     },
-    thumbnail: {
+    imgUrl: {
       type: "string",
+      default:
+        "https://images.unsplash.com/photo-1598620617137-2ab990aadd37?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2370&q=80",
     },
     videoUrl: {
       type: "string",
