882d8a3700666f62a7ffa229dafcb7ee0aef5105
AbdullahiAden <Abbe6152@gmail.com>
2023-09-27 15:33:09 +0200

    refactor

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -1,133 +1,41 @@
 import React, { useEffect } from "react";
 import "./card.scss";
-import axios from "axios";
-
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 import { useGetAllBooksQuery } from "../../slices/booksApiSlice";
-import { useGetVideos } from "../../slices/apiSlice";
 import { setBooksData } from "../../slices/bookSlice";
-import { setSingleBookData } from "../../slices/bookSlice";
 
 import Loader from "../Loader";
 
-const Card = (props) => {
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 4,
-      slidesToSlide: 3.9, // optional, default to 1.
-    },
-    tablet: {
-      breakpoint: { max: 1023, min: 464 },
-      items: 3,
-      slidesToSlide: 2.9, // optional, default to 1.
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 250 },
-      items: 2,
-      slidesToSlide: 1.9, // optional, default to 1.
-    },
-  };
-
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const params = useParams();
-
-  const { singleBookData } = useSelector((state) => state.books);
-
+const Card = () => {
   const { data, isLoading, error } = useGetAllBooksQuery();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (data) {
-      // console.log(data);
       //set data to our global state
       dispatch(setBooksData({ ...data }));
     }
   }, [data]);
 
-  const handleOnClick = async (e) => {
-    {
-      console.log(e.title);
-      console.log(e._id);
-    }
-    //set single book data
-
-    try {
-      const res = await axios.get(`/api/books/find/${e._id}`);
-      dispatch(setSingleBookData({ ...res.data }));
-    } catch (err) {
-      console.log(err.response.data);
-    }
-  };
-
-  // const getVideos = async () => {
-  //   try {
-  //     const videosRes = await getAllVideos;
-  //     console.log(videosRes);
-  //   } catch (err) {
-  //     console.log(err?.data?.message || err.error);
-  //   }
-  // };
   return (
     <div className="card-container">
       <div className="card-wrapper">
-        {/* <Carousel
-        className="vid-wrapper"
-        responsive={responsive}
-        swipeable={true}
-        draggable={true}
-        infinite={true}
-        keyBoardControl={true}
-        customTransition="all .5"
-        transitionDuration={500}
-        containerClass="carousel-contaner"
-        deviceType={props.deviceType}
-        itemClass="carousel-item-padding-40-px"
-      > */}
-
         {isLoading && <Loader />}
         {data &&
           data.map((book) => {
             return (
               <div key={book._id} className="card">
                 <Link to={`/books/${book._id}`} className="link">
-                  <img
-                    src={book.imgUrl}
-                    alt=""
-                    // onClick={() => handleOnClick(book)}
-                  />
+                  <img src={book.imgUrl} alt="" />
                   <p> {book.title}</p>
                 </Link>
               </div>
             );
           })}
-
-        {/* <div className="card">
-          <img
-            src="https://plus.unsplash.com/premium_photo-1671308539073-ebf8985a6a11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=700&q=60"
-            alt=""
-          />
-          <p>Book title </p>
-        </div>
-        <div className="card">
-          <img
-            src="https://plus.unsplash.com/premium_photo-1671308539073-ebf8985a6a11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=700&q=60"
-            alt=""
-          />
-          <p>Book title </p>
-        </div>
-        <div className="card">
-          <img
-            src="https://plus.unsplash.com/premium_photo-1671308539073-ebf8985a6a11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=700&q=60"
-            alt=""
-          />
-          <p>Book title </p>
-        </div> */}
-        {/* </Carousel> */}
       </div>
     </div>
   );
