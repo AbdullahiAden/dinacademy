7ade499d96277451d1ea65a982331d32a9e8836f
AbdullahiAden <Abbe6152@gmail.com>
2023-09-30 11:04:03 +0200

    use of localStorage to persist data on page

--- client/src/slices/bookSlice.js
+++ client/src/slices/bookSlice.js
@@ -2,9 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 import { apiSlice } from "./apiSlice";
 
 const initialState = {
-  booksData: null,
+  booksData: localStorage.getItem("booksData")
+    ? JSON.parse(localStorage.getItem("booksData"))
+    : null,
 
-  singleBookData: null,
+  singleBookData: localStorage.getItem("singleBookData")
+    ? JSON.parse(localStorage.getItem("singleBookData"))
+    : null,
   loading: true,
 };
 
@@ -14,10 +18,13 @@ const BookSlice = createSlice({
   reducers: {
     setBooksData: (state, action) => {
       state.booksData = action.payload;
+      localStorage.setItem("booksData", JSON.stringify(action.payload));
       state.loading = false;
     },
     setSingleBookData: (state, action) => {
       state.singleBookData = action.payload;
+      localStorage.setItem("singleBookData", JSON.stringify(action.payload));
+
       state.loading = false;
     },
   },
--- client/src/slices/videoSlice.js
+++ client/src/slices/videoSlice.js
@@ -2,9 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 import { apiSlice } from "./apiSlice";
 
 const initialState = {
-  videosData: null,
+  videosData: localStorage.getItem("videosData")
+    ? JSON.parse(localStorage.getItem("videosData"))
+    : null,
 
-  singleVideoData: null,
+  singleVideoData: localStorage.getItem("singleVideoData")
+    ? JSON.parse(localStorage.getItem("singleVideoData"))
+    : null,
 };
 
 const VideoSlice = createSlice({
@@ -13,9 +17,11 @@ const VideoSlice = createSlice({
   reducers: {
     setVideosData: (state, action) => {
       state.videosData = action.payload;
+      localStorage.setItem("videosData", JSON.stringify(action.payload));
     },
     setClickedVideoData: (state, action) => {
       state.singleVideoData = action.payload;
+      localStorage.setItem("singleVideoData", JSON.stringify(action.payload));
     },
   },
 });
