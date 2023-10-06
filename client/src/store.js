e76653623bd58264779ef22d85b45e32f985bdba
AbdullahiAden <Abbe6152@gmail.com>
2023-10-06 11:36:30 +0200

    localstorage for singleBookData instead of persist

--- client/src/store.js
+++ client/src/store.js
@@ -1,66 +1,66 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import authReducer from "./slices/authSlice";
-// import videoReducer from "./slices/videoSlice";
-// import bookReducer from "./slices/bookSlice";
-// import { apiSlice } from "./slices/apiSlice";
-
-// const store = configureStore({
-//   reducer: {
-//     auth: authReducer,
-//     books: bookReducer,
-//     videos: videoReducer,
-//     [apiSlice.reducerPath]: apiSlice.reducer,
-//   },
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware().concat(apiSlice.middleware),
-//   devTools: true,
-// });
-
-// export default store;
-
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import videoReducer from "./slices/videoSlice";
 import bookReducer from "./slices/bookSlice";
 import { apiSlice } from "./slices/apiSlice";
 
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-
-import storage from "redux-persist/lib/storage";
-
-const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
-};
-
-const rootReducer = combineReducers({
-  auth: authReducer,
-  books: bookReducer,
-  videos: videoReducer,
-  [apiSlice.reducerPath]: apiSlice.reducer,
-});
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-export const store = configureStore({
-  reducer: persistedReducer,
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    books: bookReducer,
+    videos: videoReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }).concat([apiSlice.middleware]),
+    getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
 
-export const persistor = persistStore(store);
+export default store;
+
+// import { configureStore, combineReducers } from "@reduxjs/toolkit";
+// import authReducer from "./slices/authSlice";
+// import videoReducer from "./slices/videoSlice";
+// import bookReducer from "./slices/bookSlice";
+// import { apiSlice } from "./slices/apiSlice";
+
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from "redux-persist";
+
+// import storage from "redux-persist/lib/storage";
+
+// const persistConfig = {
+//   key: "root",
+//   version: 1,
+//   storage,
+// };
+
+// const rootReducer = combineReducers({
+//   auth: authReducer,
+//   books: bookReducer,
+//   videos: videoReducer,
+//   [apiSlice.reducerPath]: apiSlice.reducer,
+// });
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }).concat([apiSlice.middleware]),
+//   devTools: true,
+// });
+
+// export const persistor = persistStore(store);
