9b4d696d72b84b46bc5d24ac84a9fa2efd4cd075
AbdullahiAden <Abbe6152@gmail.com>
2023-10-25 13:21:53 +0200

    add book done-without image

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Card from "../../components/card/Card";
 import "./dashboard.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import Upload from "../../components/Upload/Upload.jsx";
 
 const Dashboard = () => {
   const { userInfo } = useSelector((state) => state.auth);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [openUpload, setOpenUpload] = useState(false);
 
   useEffect(() => {
     if (!userInfo) {
@@ -20,12 +22,13 @@ const Dashboard = () => {
       <Navbar />
       <div className="top-sec">
         <h1>All of our books</h1>
-
-        {userInfo.isAdmin === true && (
-          <div className="add-book">
-            <button>add book </button>
-          </div>
+      </div>
+      <div className="upload">
+        <div>All books</div>
+        {userInfo?.isAdmin === true && (
+          <button onClick={() => setOpenUpload(true)}>add book</button>
         )}
+        {openUpload && <Upload setOpenUpload={setOpenUpload} />}
       </div>
 
       <form action=""></form>
--- client/src/components/Upload/Upload.jsx
+++ client/src/components/Upload/Upload.jsx
@@ -3,6 +3,8 @@ import "./upload.scss";
 import { useNavigate, useParams } from "react-router-dom";
 
 import { useAddVideoMutation } from "../../slices/videosApiSlice";
+import { useAddBookMutation } from "../../slices/booksApiSlice";
+
 import Loader from "../Loader";
 import { toast } from "react-toastify";
 
@@ -14,6 +16,7 @@ const Upload = ({ setOpenUpload }) => {
 
   //to get the function to call to  fire off login mutation
   const [addVideo, { isLoading }] = useAddVideoMutation();
+  const [addBook, { isUploading }] = useAddBookMutation();
 
   const handelInputChange = (e) => {
     setInputs((prev) => {
@@ -21,7 +24,7 @@ const Upload = ({ setOpenUpload }) => {
     });
   };
 
-  const handleUpload = async (e) => {
+  const handleVideoUpload = async (e) => {
     e.preventDefault();
 
     try {
@@ -39,52 +42,124 @@ const Upload = ({ setOpenUpload }) => {
       toast(err?.data?.message || err.error);
     }
   };
+  const handleBookUpload = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await addBook({
+        ...inputs,
+      }).unwrap();
+
+      setOpenUpload(false);
+      //reload page after adding book
+      window.location.reload(false);
+    } catch (err) {
+      console.log(err?.data?.message || err.error);
+      toast(err?.data?.message || err.error);
+    }
+  };
 
   return (
     <div className="upload-container">
-      <div className="wrapper">
-        <div className="close" onClick={() => setOpenUpload(false)}>
-          X
-        </div>
-        <h1 className="title">Upload A video</h1>
-        {isLoading && <Loader />}
+      {params.id ? (
+        // upload video
+        <div className="wrapper">
+          <div className="close" onClick={() => setOpenUpload(false)}>
+            X
+          </div>
+          <h1 className="title">Upload A video</h1>
+          {isLoading && <Loader />}
 
-        <div className="video-title">
-          <label htmlFor="input-url">Video Url</label>
-          <input
-            type="url"
-            required
-            name="videoUrl"
-            id="input-url"
-            className="title-input"
-            onChange={handelInputChange}
-          />
-        </div>
-        <div className="video-title">
-          <label htmlFor="input-title">Titel</label>
-          <input
-            type="text"
-            name="title"
-            id="input-title"
-            className="title-input"
-            onChange={handelInputChange}
-          />
-        </div>
-        <div className="video-desc">
-          <label htmlFor="">description</label>
-          <textarea
-            name="description"
-            id=""
-            cols="10"
-            rows="5"
-            onChange={handelInputChange}
-          ></textarea>
-        </div>
+          <div className="video-title">
+            <label htmlFor="input-url">Video Url</label>
+            <input
+              type="url"
+              required
+              name="videoUrl"
+              id="input-url"
+              className="title-input"
+              onChange={handelInputChange}
+            />
+          </div>
+          <div className="video-title">
+            <label htmlFor="input-title">Titel</label>
+            <input
+              type="text"
+              name="title"
+              id="input-title"
+              className="title-input"
+              onChange={handelInputChange}
+            />
+          </div>
+          <div className="video-desc">
+            <label htmlFor="">description</label>
+            <textarea
+              name="description"
+              id=""
+              cols="10"
+              rows="5"
+              onChange={handelInputChange}
+            ></textarea>
+          </div>
 
-        <button type="submit" className="upload-btn" onClick={handleUpload}>
-          Upload
-        </button>
-      </div>
+          <button
+            type="submit"
+            className="upload-btn"
+            onClick={handleVideoUpload}
+          >
+            Upload
+          </button>
+        </div>
+      ) : (
+        // upload Book
+        <div className="wrapper">
+          <div className="close" onClick={() => setOpenUpload(false)}>
+            X
+          </div>
+          <h1 className="title">Upload Book</h1>
+          {isLoading && <Loader />}
+
+          <div className="video-title">
+            <label htmlFor="input-url">book image</label>
+            <input
+              type="url"
+              required
+              name="videoUrl"
+              id="input-url"
+              className="title-input"
+              onChange={handelInputChange}
+            />
+          </div>
+          <div className="video-title">
+            <label htmlFor="input-title">Titel</label>
+            <input
+              type="text"
+              name="title"
+              id="input-title"
+              className="title-input"
+              onChange={handelInputChange}
+            />
+          </div>
+          <div className="video-desc">
+            <label htmlFor="">description</label>
+            <textarea
+              name="description"
+              id=""
+              cols="10"
+              rows="5"
+              onChange={handelInputChange}
+            ></textarea>
+          </div>
+
+          <button
+            type="submit"
+            className="upload-btn"
+            onClick={handleBookUpload}
+          >
+            Upload
+          </button>
+        </div>
+      )}
     </div>
   );
 };
